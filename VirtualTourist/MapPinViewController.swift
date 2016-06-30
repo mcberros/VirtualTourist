0eecee6c11c3bf7cb593c974b7b40c2d7d7d4580
mcberros <mcberros@gmail.com>
2016-06-30 20:32:09 +0200

    Add recognizer to map

--- VirtualTourist/MapPinViewController.swift
+++ VirtualTourist/MapPinViewController.swift
@@ -7,19 +7,41 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class MapPinViewController: UIViewController {
+    var annotations: [MKPointAnnotation]?
+    var coordinate: CLLocationCoordinate2D?
+    var latitude: Double?
+    var longitude: Double?
+
+    var longPressGestureRecognizer: UILongPressGestureRecognizer? = nil
+
+    @IBOutlet weak var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        annotations = [MKPointAnnotation]()
+        longPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: "addAnotation:")
+        longPressGestureRecognizer!.minimumPressDuration = 1.0
+        mapView.addGestureRecognizer(longPressGestureRecognizer!)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        //Get Pins from DB
     }
-
-
 }
+
+extension MapPinViewController {
+    func addAnotation(recognizer: UIGestureRecognizer) {
+        let touchPoint = recognizer.locationInView(mapView)
+        let newCoordinates = mapView.convertPoint(touchPoint, toCoordinateFromView: mapView)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = newCoordinates
+        mapView.addAnnotation(annotation)
+    }
+}
+
 
