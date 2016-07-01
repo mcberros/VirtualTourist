f3e2faf0781730d1ec343d203bf46f0c8a71a044
mcberros <mcberros@gmail.com>
2016-07-01 21:59:20 +0200

    Drop a pin and tap to the Photo Album

--- VirtualTourist/MapPinViewController.swift
+++ VirtualTourist/MapPinViewController.swift
@@ -16,31 +16,54 @@ class MapPinViewController: UIViewController {
     var longitude: Double?
 
     var longPressGestureRecognizer: UILongPressGestureRecognizer? = nil
+    var tapRecognizer: UITapGestureRecognizer? = nil
 
     @IBOutlet weak var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         annotations = [MKPointAnnotation]()
-        longPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: "addAnotation:")
+        //Add GestureRecognizer to Drop a pin
+        longPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: "addAnnotation:")
         longPressGestureRecognizer!.minimumPressDuration = 1.0
         mapView.addGestureRecognizer(longPressGestureRecognizer!)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        mapView.delegate = self
         //Get Pins from DB
     }
 }
 
+extension MapPinViewController: MKMapViewDelegate {
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        let reuseId = "Pin"
+
+        var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as? MKPinAnnotationView
+
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.pinTintColor = UIColor.blueColor()
+        } else {
+            pinView!.annotation = annotation
+        }
+
+        return pinView
+    }
+
+    func mapView(mapView: MKMapView, didSelectAnnotationView view: MKAnnotationView) {
+        let photoAlbumViewController = self.storyboard?.instantiateViewControllerWithIdentifier("PhotoAlbumID") as! PhotoAlbumViewController
+
+        self.navigationController?.pushViewController(photoAlbumViewController, animated: true)
+    }
+}
+
 extension MapPinViewController {
-    func addAnotation(recognizer: UIGestureRecognizer) {
+    func addAnnotation(recognizer: UIGestureRecognizer) {
         let touchPoint = recognizer.locationInView(mapView)
         let newCoordinates = mapView.convertPoint(touchPoint, toCoordinateFromView: mapView)
         let annotation = MKPointAnnotation()
         annotation.coordinate = newCoordinates
         mapView.addAnnotation(annotation)
+        //Persist pin
+        annotations!.append(annotation)
     }
 }
 
---
+++ VirtualTourist/PhotoAlbumViewController.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoAlbumViewController.swift
+//  VirtualTourist
+//
+//  Created by Carmen Berros on 30/06/16.
+//  Copyright © 2016 mcberros. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class PhotoAlbumViewController: UIViewController {
+
+    @IBOutlet weak var pinMapView: MKMapView!
+    @IBOutlet weak var photoAlbumCollectionView: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //Center pin
+        //Show photoAlbum
+        //If there are no photos for pin connect Flickr
+        //If there are photos, get photos for this pin
+    }
+}
